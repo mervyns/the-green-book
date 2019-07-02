7cf8620717d25e7e52078928bed8ef7671e3ff99
Mervyn Tan <mervyn@sourcesage.co>
2019-07-02 17:31:35 +0800

    changed logo and removed useless blogs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/thciq.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -63,14 +63,14 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Products
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
